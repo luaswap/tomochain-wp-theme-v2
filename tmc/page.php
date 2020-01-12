9c4e90562eea640f14c995b63e9b80c102e786b4
tinhbeng <tinhns@vietbrain.com>
2020-01-13 02:01:12 +0700

    Add Event, Team Member

--- tmc/page.php
+++ tmc/page.php
@@ -17,22 +17,20 @@ get_header();
 	
 <main id="main" class="site-main">
 	<div class="container">
-		<div class="row">
-			<div class="site-content">
-			<?php
-				while ( have_posts() ) :
-					the_post();
+		<div class="site-content">
+		<?php
+			while ( have_posts() ) :
+				the_post();
 
-					get_template_part( 'template-parts/content', 'page' );
+				get_template_part( 'template-parts/content', 'page' );
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) :
-						comments_template();
-					endif;
+				// If comments are open or we have at least one comment, load up the comment template.
+				if ( comments_open() || get_comments_number() ) :
+					comments_template();
+				endif;
 
-				endwhile; // End of the loop.
-			?>
-			</div>
+			endwhile; // End of the loop.
+		?>
 		</div>
 	</div>
 </main><!-- #main -->
