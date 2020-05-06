46153e0303ee879a80b912cd5549b7bb13d09433
tinhbeng <tinhns@vietbrain.com>
2020-05-06 23:10:24 +0700

    Add shortcode Countdown

--- tmc-addon/tmc-addon.php
+++ tmc-addon/tmc-addon.php
@@ -70,6 +70,9 @@ if ( ! class_exists( 'TMC_Addon' ) ) {
 	    	require_once TMC_ADDON_INCLUDES_DIR . '/post-type/usecase.php';
 	    	require_once TMC_ADDON_INCLUDES_DIR . '/post-type/press.php';
 
+	    	/* Include Shortcode*/
+	    	require_once TMC_ADDON_INCLUDES_DIR . '/shortcode/shortcodes.php';
+
 	    	/* Include Elementor Widget */
 	    	require_once TMC_ADDON_INCLUDES_DIR . '/class-elementor-extra-widgets.php';
 	    	
